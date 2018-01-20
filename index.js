caf5a34273784b47260367cc5cb361f5d5e43de5
r3mis4 <r3mi4a@yahoo.com>
2018-01-21 00:27:46 +0330

    Switch to https

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const https = require('https')
+const privateKey = fs.readFileSync('key.pem')
+const certificate = fs.readFileSync('certificate.pem')
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(4500)
 app.get('/', (req, res) => {
   let ip = req.ip
   res.send(ip)
